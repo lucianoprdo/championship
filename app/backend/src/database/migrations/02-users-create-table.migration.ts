e7fb88e51436eaadbb5ddcaae1c7887d5c2eb16a
Luciano Prado <lucianobatuta@live.com>
2024-05-19 18:51:06 -0300

    feat: create migrations for users

---
+++ app/backend/src/database/migrations/02-users-create-table.migration.ts
@@ -0,0 +1,38 @@
+import { DataTypes, Model, QueryInterface } from 'sequelize';
+import { IUsers} from '../../Interfaces/users/IUsers';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IUsers>>('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'username',
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'role',
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'email',
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'password',
+      },
+    });
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('users');
+  },
+};
