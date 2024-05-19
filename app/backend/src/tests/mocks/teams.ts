83766e6b3f8b16505b633d267c3dbe811338993e
Luciano Prado <lucianobatuta@live.com>
2024-05-19 13:04:44 -0300

    feat: Create mocks for unit testing in teams

---
+++ app/backend/src/tests/mocks/teams.ts
@@ -0,0 +1,24 @@
+const allTeamsFromDB = [
+  {
+    id: 1,
+    teamName: 'Avaí/Kindermann',
+  },
+  {
+    id: 2,
+    teamName: 'Bahia',
+  },
+  {
+    id: 3,
+    teamName: 'Botafogo',
+  },
+];
+
+const teamFromDB = {
+  id: 3,
+  teamName: 'Botafogo',
+};
+
+export default {
+  allTeamsFromDB,
+  teamFromDB,
+};
