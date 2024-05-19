e38ec0b98401e5dc675656f3aab323e0dd82358c
Luciano Prado <lucianobatuta@live.com>
2024-05-19 16:51:10 -0300

    feat: implements function to search for id in the service layer

--- app/backend/src/services/TeamsService.ts
+++ app/backend/src/services/TeamsService.ts
@@ -1,11 +1,22 @@
 import TeamsModel, { TeamsSequelizeModel } from '../database/models/TeamsModel';
 import { ServiceResponse } from '../Interfaces/ServiceResponse';
 
-async function getAllTeams(): Promise<ServiceResponse<TeamsSequelizeModel[]>> {
+async function getAllService(): Promise<
+ServiceResponse<TeamsSequelizeModel[]>
+> {
   const teamsDb = await TeamsModel.findAll();
-  return { status: 200, data: teamsDb };
+  return { status: 'SUCCESSFUL', data: teamsDb };
+}
+
+async function findByIdService(
+  id: number,
+): Promise<ServiceResponse<TeamsSequelizeModel | null>> {
+  const teamsIdDb = await TeamsModel.findByPk(Number(id));
+
+  return { status: 'SUCCESSFUL', data: teamsIdDb };
 }
 
 export default {
-  getAllTeams,
+  getAllService,
+  findByIdService,
 };
