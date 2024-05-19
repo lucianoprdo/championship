cab0b8c8b73441ab62703698fef5f10ae107a33c
Luciano Prado <lucianobatuta@live.com>
2024-05-19 16:49:40 -0300

    feat: implements function to search for id in the controller layer

--- app/backend/src/controllers/TeamsController.ts
+++ app/backend/src/controllers/TeamsController.ts
@@ -1,11 +1,26 @@
 import { Request, Response } from 'express';
 import teamsService from '../services/TeamsService';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
 
-async function getAllTeamsController(_req: Request, res: Response) {
-  const serviceTeams = await teamsService.getAllTeams();
-  return res.status(serviceTeams.status).json(serviceTeams.data);
+async function getAllController(_req: Request, res: Response) {
+  const { status, data } = await teamsService.getAllService();
+
+  const code = mapStatusHTTP(status);
+
+  return res.status(code).json(data);
+}
+
+async function getByIdController(req: Request, res: Response) {
+  const { id } = req.params;
+
+  const { status, data } = await teamsService.findByIdService(Number(id));
+
+  const code = mapStatusHTTP(status);
+
+  return res.status(code).json(data);
 }
 
 export default {
-  getAllTeamsController,
+  getAllController,
+  getByIdController,
 };
