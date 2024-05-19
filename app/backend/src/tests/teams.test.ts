cc35e64c0d2958a58e3ef0610d68863abd8c56fe
Luciano Prado <lucianobatuta@live.com>
2024-05-19 15:49:34 -0300

    refactor: fixes test with status 200 return and implements test for GET /user/:id

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,30 +1,41 @@
-import sinon from 'sinon';
-import chai, { expect } from 'chai';
-import chaiHttp from 'chai-http';
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
 
-import teamsMocks from './mocks/teams';
+import { App } from '../app';
+import TeamsMocks from '../database/models/TeamsModel';
+import mocks from './mocks/teams';
 
 chai.use(chaiHttp);
 
-import { app } from '../app';
+const { expect } = chai;
+
+const { app } = new App();
 
 describe('GET /teams', function () {
   beforeEach(function () {
     sinon.restore();
   });
 
-  it('Must return status 200 and all teams in the database', async function () {
-    // Arrange
-    const responseBodyTeams = teamsMocks.allTeamsFromDB;
+  it('It should return a list of teams with status 200', async () => {
+    sinon.stub(TeamsMocks, 'findAll').resolves(mocks.allTeamsFromDB as any);
 
-    // Act
-    const httpResponse = await chai
-      .request(app)
-      .get('/teams')
-      .send(responseBodyTeams);
+    const { status, body } = await chai.request(app).get('/teams');
 
-    // Assert
-    expect(httpResponse.status).to.equal(200);
-    expect(httpResponse.body).to.deep.equal(responseBodyTeams);
+    expect(status).to.equal(200);
+    expect(body).to.be.an('array');
   });
+
+  it('Should return only one team with status 200', async () => {
+    sinon.stub(TeamsMocks, 'findByPk').resolves(mocks.teamFromDB as any);
+
+    const { status, body } = await chai.request(app).get('/teams/3');
+
+    expect(status).to.equal(200);
+    expect(body).to.have.property('id', 3);
+    expect(body).to.have.property('teamName', 'Botafogo');
+  });
+
+  afterEach(sinon.restore);
 });
