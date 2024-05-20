68388f161108de220abcca8126b9686c8ce4fe23
Luciano Prado <lucianobatuta@live.com>
2024-05-20 18:31:34 -0300

    refactor: matches migration with cascade

--- app/backend/src/database/migrations/03-matches-create-table.migration.ts
+++ app/backend/src/database/migrations/03-matches-create-table.migration.ts
@@ -6,51 +6,49 @@ export default {
     return queryInterface.createTable<Model<IMatch>>('matches', {
       id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         primaryKey: true,
+        allowNull: false,
         autoIncrement: true,
       },
       homeTeamId: {
+        allowNull: false,
         field: 'home_team_id',
         type: DataTypes.INTEGER,
-
-        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
         references: {
           model: 'teams',
           key: 'id',
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
       },
       homeTeamGoals: {
-        type: DataTypes.STRING,
         allowNull: false,
         field: 'home_team_goals',
+        type: DataTypes.INTEGER,
       },
       awayTeamId: {
-        type: DataTypes.STRING,
         allowNull: false,
         field: 'away_team_id',
+        type: DataTypes.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
         references: {
           model: 'teams',
           key: 'id',
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
       },
       awayTeamGoals: {
-        type: DataTypes.STRING,
         allowNull: false,
         field: 'away_team_goals',
+        type: DataTypes.INTEGER,
       },
       inProgress: {
-        type: DataTypes.STRING,
         allowNull: false,
         field: 'in_progress',
+        type: DataTypes.BOOLEAN,
       },
     });
   },
-
   down(queryInterface: QueryInterface) {
     return queryInterface.dropTable('matches');
   },
