add3e88e60a76607546583d81041ca1c8297fffe
Luciano Prado <lucianobatuta@live.com>
2024-05-19 22:25:19 -0300

    feat: implements validations on the users route

--- app/backend/src/router/users.router.ts
+++ app/backend/src/router/users.router.ts
@@ -1,12 +1,13 @@
 import { Request, Router, Response } from 'express';
 import UsersController from '../controllers/UsersController';
+import Validations from '../middlewares/validations';
 
 const usersController = new UsersController();
 
 const usersRouter = Router();
 
-usersRouter.post('/', (req: Request, res: Response) => usersController.login(req, res));
+usersRouter.post('/', Validations.validateLogin, (req: Request, res: Response) => usersController.login(req, res));
 
-usersRouter.get('/role', (req: Request, res: Response) => usersController.getRole(req, res));
+usersRouter.get('/role', Validations.validateToken, (req: Request, res: Response) => usersController.getRole(req, res));
 
 export default usersRouter;
