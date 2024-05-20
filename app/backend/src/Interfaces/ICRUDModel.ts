7b355c9ae9dafef8aceda605b196599fd80bbaf7
Luciano Prado <lucianobatuta@live.com>
2024-05-20 19:03:43 -0300

    feat: implements ICRUDModelCreator

--- app/backend/src/Interfaces/ICRUDModel.ts
+++ app/backend/src/Interfaces/ICRUDModel.ts
@@ -1,5 +1,9 @@
 import IExample from './IExample';
 
+export interface ICRUDModelCreator<T> {
+  create(data: Partial<T>): Promise<T>;
+}
+
 export interface ICRUDModelReader<T> {
   findAll(): Promise<T[]>;
   findById(id: IExample): Promise<T | null>;
