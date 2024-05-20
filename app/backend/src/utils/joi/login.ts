7edbb98210778748581ff06e43557feb5f939de2
Luciano Prado <lucianobatuta@live.com>
2024-05-19 22:19:38 -0300

    feat: Create joi to validate login

---
+++ app/backend/src/utils/joi/login.ts
@@ -0,0 +1,20 @@
+import * as Joi from 'joi';
+
+const minimumRes = 'UNAUTHORIZED | Invalid email or password';
+
+const joiValidateLogin = (input: Record<string, string>) => {
+  const schema = Joi.object().keys({
+    email: Joi.string().email().min(3).required()
+      .messages({
+        'any.required': 'INVALID_DATA|All fields must be filled',
+        'string.email': 'UNAUTHORIZED|Invalid email or password',
+      }),
+    password: Joi.string().min(6).required().messages({
+      'any.required': 'INVALID_DATA | All fields must be filled',
+      'string.min': minimumRes,
+    }),
+  });
+  return schema.validate(input);
+};
+
+export default joiValidateLogin;
