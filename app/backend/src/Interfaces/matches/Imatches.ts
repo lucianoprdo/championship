4fe760cd8f684f35a7c7df40541328d8f929976f
Luciano Prado <lucianobatuta@live.com>
2024-05-20 15:26:32 -0300

    feat: creates IMatches interface

---
+++ app/backend/src/Interfaces/matches/Imatches.ts
@@ -0,0 +1,15 @@
+import IExample from "../IExample";
+
+export interface IHomeTeam {
+    homeTeamId: number;
+    homeTeamGoals: number;
+  }
+  
+  export interface IAwayTeam {
+    awayTeamId: number;
+    awayTeamGoals: number;
+  }
+  
+  export interface IMatch extends IExample, IHomeTeam, IAwayTeam{
+    inProgress: number;
+  }
