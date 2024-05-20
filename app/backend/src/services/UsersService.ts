8b29f5078128b7b8d15c0c3568e700fc843e8ca9
Luciano Prado <lucianobatuta@live.com>
2024-05-19 21:31:52 -0300

    feat: creates UsersService

---
+++ app/backend/src/services/UsersService.ts
@@ -0,0 +1,45 @@
+import * as bcrypt from 'bcryptjs';
+import { IUser } from '../Interfaces/users/IUsers';
+import UsersModel from '../database/models/UsersModel';
+import {
+  ServiceResponse,
+  ServiceResponseError,
+} from '../Interfaces/ServiceResponse';
+import JWT from '../utils/JWT';
+
+export default class UsersService {
+  private invalidDataResponse: ServiceResponseError = {
+    status: 'UNAUTHORIZED',
+    data: { message: 'Invalid email or password' },
+  };
+
+  constructor(
+    private usersModel: any = new UsersModel(),
+    private jwtService = JWT,
+  ) {}
+
+  public async login(
+    email: string,
+    password: string,
+  ): Promise<ServiceResponse<IUser | { token: string }>> {
+    const user = await this.usersModel.findByEmail(email);
+
+    if (!user) return this.invalidDataResponse;
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) return this.invalidDataResponse;
+
+    const token = this.jwtService.sign({ email: user.email });
+
+    return { status: 'SUCCESSFUL', data: { token } };
+  }
+
+  public async getRole(
+    email: string,
+  ): Promise<ServiceResponse<{ role: string }>> {
+    const user = await this.usersModel.findByEmail(email);
+
+    return { status: 'SUCCESSFUL', data: { role: user?.role as string } };
+  }
+}
