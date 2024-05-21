0ed9f0aad63216284f6ca7f0174b26ff60af2312
Luciano Prado <lucianobatuta@live.com>
2024-05-21 10:21:41 -0300

    feat: implements all functions necessaries

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -1,16 +1,31 @@
 import { Request, Response } from 'express';
 import MatchesService from '../services/MatchesService';
 
-const getAllMatches = async (
-  req: Request,
-  res: Response,
-): Promise<Response> => {
-  const service = new MatchesService();
-  const { status, data } = await service.getAllMatches();
+export default class MatchesController {
+  constructor(private matchesService = new MatchesService()) {}
 
-  return res.status(Number(status)).json(data);
-};
+  public async getAllMatches(req: Request, res: Response): Promise<Response> {
+    const { inProgress } = req.query;
+    const matches = await this.matchesService.getAllMatches(inProgress as string);
+    return res.status(200).json(matches.data);
+  }
 
-export default {
-  getAllMatches,
-};
+  public async finishMatch(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const result = await this.matchesService.finishMatch(id);
+    return res.status(200).json(result.data);
+  }
+
+  public async updateMatch(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const match = req.body;
+    const result = await this.matchesService.updateMatch(id, match);
+    return res.status(200).json(result.data);
+  }
+
+  public async insertNewMatch(req: Request, res: Response): Promise<Response> {
+    const match = req.body;
+    const result = await this.matchesService.insertNewMatch(match);
+    return res.status(result.status === 'CREATED' ? 201 : 400).json(result.data);
+  }
+}
