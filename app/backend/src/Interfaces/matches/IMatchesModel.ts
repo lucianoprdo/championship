3f5d72ed091dfe9a4bf8b7e25b288067f1de5ff7
Luciano Prado <lucianobatuta@live.com>
2024-05-21 09:41:31 -0300

    feat: creates IMatchesModel

---
+++ app/backend/src/Interfaces/matches/IMatchesModel.ts
@@ -0,0 +1,12 @@
+import { ICRUDModelCreator, ICRUDModelReader } from '../ICRUDModel';
+import { IMatch } from './IMatches';
+
+export type IMatchesModel = ICRUDModelReader<IMatch> &
+  ICRUDModelCreator<IMatch> & {
+    findAllInProgress(inProgress: string): Promise<IMatch[]>;
+    finishMatch(id: string): Promise<void>;
+    updateMatch(
+      id: string,
+      match: { homeTeamGoals: number; awayTeamGoals: number },
+    ): Promise<void>;
+  };
