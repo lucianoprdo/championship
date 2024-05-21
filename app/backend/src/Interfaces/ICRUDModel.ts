3d16ef09bb92467a3a9ad90a8273c6e42f7c8a91
Luciano Prado <lucianobatuta@live.com>
2024-05-21 09:43:34 -0300

    feat: implements updater and deleter in ICRUD

--- app/backend/src/Interfaces/ICRUDModel.ts
+++ app/backend/src/Interfaces/ICRUDModel.ts
@@ -6,5 +6,19 @@ export interface ICRUDModelCreator<T> {
 
 export interface ICRUDModelReader<T> {
   findAll(): Promise<T[]>;
-  findById(id: IExample): Promise<T | null>;
+  findById(id: number): Promise<T | null>; // Aqui mudamos o tipo do parâmetro para number
 }
+
+export interface ICRUDModelUpdater<T> {
+  update(id: number, data: Partial<T>): Promise<T | null>; // Aqui mudamos o tipo do parâmetro para number
+}
+
+export interface ICRUDModelDeleter {
+  delete(id: number): Promise<number>; // Aqui mudamos o tipo do parâmetro para number
+}
+
+export interface ICRUDModel<T>
+  extends ICRUDModelCreator<T>,
+    ICRUDModelReader<T>,
+    ICRUDModelUpdater<T>,
+    ICRUDModelDeleter {}
