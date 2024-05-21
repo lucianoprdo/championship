656ff1d9b729b05a4c147935718d7d6a96c08a3f
Luciano Prado <lucianobatuta@live.com>
2024-05-21 09:39:37 -0300

    feat: creates MatchesService com getAllMatches

---
+++ app/backend/src/services/MatchesService.ts
@@ -0,0 +1,13 @@
+import { IMatchesModel } from '../Interfaces/matches/IMatchesModel';
+import ModelMatches from '../models//ModelMatches';
+import { IMatch } from '../Interfaces/matches/IMatches';
+import { ServiceResponse } from '../Interfaces/ServiceResponse';
+
+export default class MatchesService {
+  constructor(private matchesModel: any = new ModelMatches()) {}
+
+  public async getAllMatches(): Promise<ServiceResponse<IMatch[]>> {
+    const matches = await this.matchesModel.findAll();
+    return { status: 'SUCCESSFUL', data: matches };
+  }
+}
