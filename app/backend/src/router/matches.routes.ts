0c66b31a1fa98d8d8873f18a2c32d9da39cb2475
Luciano Prado <lucianobatuta@live.com>
2024-05-22 10:31:50 -0300

    refactor: insert next parameter in the POST /matches route

--- app/backend/src/router/matches.routes.ts
+++ app/backend/src/router/matches.routes.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import Validations from '../middlewares/validations';
 import MatchesController from '../controllers/MatchesController';
 
@@ -7,13 +7,19 @@ const matchesController = new MatchesController();
 const router = Router();
 
 router.get('/', (req: Request, res: Response) =>
-  matchesController.getAllMatches(req, res));
+  matchesController.getAllMatches(req, res),
+);
 
-router.post('/', Validations.validateToken, (req: Request, res: Response) =>
-  matchesController.insertNewMatch(req, res));
+router.post(
+  '/',
+  Validations.validateToken,
+  (req: Request, res: Response, next: NextFunction) =>
+    matchesController.insertNewMatch(req, res, next),
+);
 
 router.patch('/:id', Validations.validateToken, (req: Request, res: Response) =>
-  matchesController.updateMatch(req, res));
+  matchesController.updateMatch(req, res),
+);
 
 router.patch(
   '/:id/finish',
